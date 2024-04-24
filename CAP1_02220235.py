fab7a49bb40c11b6ff3f98fa9112c6ee7448ab9a
Alushobzay05 <02220235.cst@rub.bt>
2024-04-24 21:47:44 +0600

    change to cap1

--- CAP1_02220235.py
+++ CAP1_02220235.py
@@ -13,25 +13,33 @@
 # Put your number here
 ################################
 
-# Read input from a file and return a list of tuples containing shape and outcome
 def read_input(input_file):
     game_round = []
-    with open(input_file, 'r') as file:
-        for line in file:
-            shape, outcome = line.strip().split()
-            game_round.append((shape, outcome))
+    try:
+        with open(input_file, 'r') as file:
+          for line in file:
+                shape, outcome = line.strip().split()
+                game_round.append((shape, outcome))
+    except FileNotFoundError:
+        print("Error: Input file not found.")
+        return None
+    except ValueError:
+        print("Error: Input file is not formatted correctly.")
+        return None
     return game_round
 
-# Calculate the total score of the game based on the provided rules
 def calculate_score(game_round):
+    if game_round is None:
+        return 0
     scores = {"AX": 3, "AY": 4, "AZ": 8, "BX": 1, "BY": 5, "BZ": 9, "CX": 2, "CY": 6, "CZ": 7}
     total_score = sum(scores.get(shape + outcome, 0) for shape, outcome in game_round)
     return total_score
 
-# Main function to run the program
 def main():
     input_file = "CSF101-CAP/input_5_cap1.txt"
     game_round = read_input(input_file)
-    total_round_score = calculate_score(game_round)
-    print("Total Score:", total_round_score)
-
+    if game_round is not None:
+        total_round_score = calculate_score(game_round)
+        print("Total Score:", total_round_score)
+if __name__ == "__main__":
+    main()
